3d28848a15744cd17441d8a315c5d09541b6bb1b
Shana Russell <shana0515@gmail.com>
2023-06-03 18:09:30 -0500

    changed firebase database

--- src/GroceryList.jsx
+++ src/GroceryList.jsx
@@ -13,12 +13,12 @@ import {
 } from "firebase/firestore";
 
 const style = {
-  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
-  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+  bg: `h-screen w-screen p-4 bg-black`,
+  container: `bg-slate-100 max-w-[400px] w-full rounded-md shadow-xl p-4`,
   heading: `text-3xl font-bold text-center text-gray-800 p-2`,
   form: `flex justify-between`,
   input: `border p-2 w-full text-xl`,
-  button: `border p-4 ml-2 bg-purple-500 text-slate-100`,
+  button: `border p-4 ml-2 bg-[#2EB62C] text-slate-100`,
   count: `text-center p-2`,
 };
 
