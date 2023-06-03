3efcd44683ecb66478717f0ce68a5152cbb01505
Shana Russell <shana0515@gmail.com>
2023-06-03 15:52:38 -0500

    simple list no logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
 import { useState } from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+import Todo from "./Todo";
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
+  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+  heading: `text-3xl font-bold text-center text-gray-800 p-2`,
+  form: `flex justify-between`,
+  input: `border p-2 w-full text-xl`,
+  button: `border p-4 ml-2 bg-purple-500 text-slate-100`,
+};
 
 function App() {
+  const [todos, setTodos] = useState(["learn react", "do leetcode"]);
+
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}>Todo App</h3>
+        <form className={style.form}>
+          <input className={style.input} type="text" placeholder="Add Todo" />
+          <button className={style.button}>
+            <AiOutlinePlus size={30} />
+          </button>
+        </form>
+        <ul>
+          {todos.map((todo, index) => (
+            <Todo key={index} todo={todo} />
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
 
