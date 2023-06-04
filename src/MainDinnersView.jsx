6c491258f2f8968940a8f270c132946ea9f8ac0c
Shana Russell <shana0515@gmail.com>
2023-06-03 20:48:25 -0500

    can delete a dinner

--- src/MainDinnersView.jsx
+++ src/MainDinnersView.jsx
@@ -9,12 +9,14 @@ import {
   addDoc,
   deleteDoc,
 } from "firebase/firestore";
+import { FaRegTrashAlt } from "react-icons/fa";
 
 const style = {
   container: `bg-slate-100 rounded-md shadow-xl p-4 mr-8 mt-8`,
   heading: `text-3xl font-bold text-center text-gray-800 p-2`,
   dinnerGrid: `flex justify-between`,
   singleDinnerContainer: `border p-4 w-[15%] text-center`,
+  trashCan: ``,
 };
 
 const MainDinnersView = () => {
@@ -34,6 +36,12 @@ const MainDinnersView = () => {
     return () => unsubscribe();
   }, []);
 
+  //handle trash click (delete)
+
+  const handleTrashClick = async (id) => {
+    await deleteDoc(doc(db, "selected-dinners", id));
+  };
+
   return (
     <div className={style.container}>
       <h3 className={style.heading}>Weekly Dinners</h3>
@@ -41,6 +49,12 @@ const MainDinnersView = () => {
         {selectedDinners.map((day) => (
           <div className={style.singleDinnerContainer} key={day.id}>
             {day.recipeTitle}
+            <div
+              className={style.trashCan}
+              onClick={() => handleTrashClick(day.id)}
+            >
+              <FaRegTrashAlt />
+            </div>
           </div>
         ))}
       </div>
