b41d29779c69d88842091c706e77b60a7bb664b8
Shana Russell <shana0515@gmail.com>
2023-06-03 20:05:26 -0500

    reads selected dinners from firebase

--- src/MainDinnersView.jsx
+++ src/MainDinnersView.jsx
@@ -1,21 +1,50 @@
+import { useState, useEffect } from "react";
+import { db } from "./firebase";
+import {
+  query,
+  collection,
+  onSnapshot,
+  updateDoc,
+  doc,
+  addDoc,
+  deleteDoc,
+} from "firebase/firestore";
+
 const style = {
   container: `bg-slate-100 rounded-md shadow-xl p-4 mr-8 mt-8`,
   heading: `text-3xl font-bold text-center text-gray-800 p-2`,
-  form: `flex justify-between`,
-  input: `border p-2 w-full text-xl`,
-  button: `border p-4 ml-2 bg-[#2EB62C] text-slate-100`,
-  count: `text-center p-2`,
+  dinnerGrid: `flex justify-between`,
+  singleDinnerContainer: `border p-4 w-[15%] text-center`,
 };
 
 const MainDinnersView = () => {
+  const [selectedDinners, setSelectedDinners] = useState([]);
+
+  // Read Selected Dinners from Firebase
+  useEffect(() => {
+    const q = query(collection(db, "selected-dinners"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let selectedDinnersArr = [];
+      querySnapshot.forEach((item) => {
+        selectedDinnersArr.push({ ...item.data(), id: item.id });
+      });
+
+      setSelectedDinners(selectedDinnersArr);
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
-   
-    
-        <div className={style.container}>
-          <h3 className={style.heading}>Weekly Dinners</h3>
-        </div>
-    
-   
+    <div className={style.container}>
+      <h3 className={style.heading}>Weekly Dinners</h3>
+      <div className={style.dinnerGrid}>
+        {selectedDinners.map((day) => (
+          <div className={style.singleDinnerContainer} key={day.id}>
+            {day.recipeTitle}
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
