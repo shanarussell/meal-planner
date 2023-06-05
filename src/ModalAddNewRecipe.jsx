fdd5346eb8b9b1733f946a312c237fc87b6233bf
Shana Russell <shana0515@gmail.com>
2023-06-05 18:26:05 -0500

    image upload added to new recipe page

--- src/ModalAddNewRecipe.jsx
+++ src/ModalAddNewRecipe.jsx
@@ -1,4 +1,3 @@
-
 import PropTypes from "prop-types";
 import { useState, useEffect } from "react";
 import { db } from "./firebase";
@@ -11,6 +10,8 @@ import {
   addDoc,
   deleteDoc,
 } from "firebase/firestore";
+import {storage} from "./firebase"
+import {ref, uploadBytes} from "firebase/storage";
 
 const style = {
   modalPosition: `justify-center items-center flex fixed inset-0 z-50 outline-none`,
@@ -26,9 +27,17 @@ const style = {
   inputIngredient: `border p-2 w-full text-xl mb-4`,
   inputInstructions: `border p-2 w-full text-xl mb-4`,
   inputNotes: `border p-2 w-full text-xl mb-4`,
-  submitButton: `bg-[#2EB62C] text-white active:bg-pink-600 font-bold uppercase text-sm mt-2 px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150`,
+  submitButton: `bg-[#2EB62C] text-white active:bg-pink-600 font-bold uppercase text-sm mt-2 px-6 py-3 rounded-lg shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150`,
   checkboxesContainer: `flex flex-col`,
   checkboxes: `text-lg font-bold text-left text-gray-800`,
+  imageUploadContainer: `flex flex-col bg-slate-200 p-5`,
+  imageFileUpload: `block w-full text-sm text-slate-500
+      file:mr-4 file:py-2 file:px-4
+      file:rounded-lg file:border-0
+      file:text-sm file:font-semibold
+      file:bg-[#2EB62C] file:text-white
+      hover:file:bg-violet-100`,
+  imageUploadButton: `bg-[#2EB62C] text-white active:bg-pink-600 font-bold uppercase text-sm mt-2 px-6 py-3 rounded-lg shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150`,
 };
 
 const ModalAddNewRecipe = ({ setNewRecipeModal }) => {
@@ -39,6 +48,7 @@ const ModalAddNewRecipe = ({ setNewRecipeModal }) => {
   const [inputIngredient, setIngredientInput] = useState("");
   const [inputInstructions, setInstructionsInput] = useState("");
   const [inputNotes, setNotesInput] = useState("");
+  const [imageUpload, setImageUpload] = useState(null);
   let isDinner = false;
   let isBreakfast = false;
   let isLunch = false;
@@ -60,6 +70,24 @@ const ModalAddNewRecipe = ({ setNewRecipeModal }) => {
       isLunch: isLunch,
     });
     setNewRecipeModal(false);
+  };
+
+  //upload image to firebase
+  //make sure image isn't empty
+  //make sure recipe has a title
+  //upload using the recipe name as the path
+  const uploadImage = (event) => {
+    event.preventDefault(event);
+    if (imageUpload == null) return;
+    if (inputRecipeName === ""){
+      alert("Please enter a recipe name before uploading a photo")
+      return;
+    }
+//change this path so the folder is the recipe name
+    const imageRef = ref(storage, `${inputRecipeName}/${imageUpload.name}`);
+    uploadBytes(imageRef, imageUpload).then(() => {
+      alert("Image Uploaded")
+    })
   };
 
   return (
@@ -114,12 +142,25 @@ const ModalAddNewRecipe = ({ setNewRecipeModal }) => {
                     rows={"7"}
                     placeholder="Add Notes with each step separated by a comma"
                   />
+                  <div className={style.imageUploadContainer}>
+                    <input
+                      className={style.imageFileUpload}
+                      onChange={(event) => {setImageUpload(event.target.files[0])}}
+                      type="file"
+                    ></input>
+                    <button
+                      className={style.imageUploadButton}
+                      onClick={uploadImage}
+                    >
+                      Upload Image
+                    </button>
+                  </div>
                   <div className={style.checkboxesContainer}>
                     <label className={style.checkboxes}>
                       <input
                         type="checkbox"
                         value={isDinner}
-                        onChange={() => isDinner = true}
+                        onChange={() => (isDinner = true)}
                         className={style.checkboxes}
                       />{" "}
                       Add to this weeks dinners?
@@ -128,7 +169,7 @@ const ModalAddNewRecipe = ({ setNewRecipeModal }) => {
                       <input
                         type="checkbox"
                         value={isBreakfast}
-                        onChange={() => isBreakfast = true}
+                        onChange={() => (isBreakfast = true)}
                         className={style.checkboxes}
                       />{" "}
                       Add to this weeks breakfasts?
@@ -137,7 +178,7 @@ const ModalAddNewRecipe = ({ setNewRecipeModal }) => {
                       <input
                         type="checkbox"
                         value={isLunch}
-                        onChange={() => isLunch = true}
+                        onChange={() => (isLunch = true)}
                         className={style.checkboxes}
                       />{" "}
                       Add to this weeks lunches?
